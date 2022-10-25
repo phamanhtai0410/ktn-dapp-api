3a7e9ae8fd0fd7a799617170388715457eafb337
khoabd <khoabd98@gmail.com>
2022-10-25 15:34:37 +0700

    [FIX] num_of_page

--- models/point.py
+++ models/point.py
@@ -22,8 +22,9 @@ class PointDao(DaoModel):
         _event_key = self.key_of_event(event)
         _rank = self.redis.zrevrange(_event_key, _start, _end - 1, withscores=True) or []
         _total = self.redis.zcard(_event_key) or 0
-        num_of_page = (_total / page_size) + 1
-
+        num_of_page = int(_total / page_size)
+        if (_total % page_size) > 0:
+            num_of_page = num_of_page + 1
         return [{
             'rank': _start + _ind + 1 if val[1] > 0 else -1,
             'point': val[1],
